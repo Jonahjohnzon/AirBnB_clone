8bc0f4b93181f6bd9cf1e9a98ba52a484f981ac9
jonah ominyi <jonahjohnzon@gmail.com>
2023-11-07 03:36:24 -0800

    basemodel

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""__init__"""
+from models.engine.file_storage import FileStorage
+
+filestorage = FileStorage()
+filestorage.reload()
---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""BaseModel class"""
+
+import models
+from datetime import datetime
+from uuid import uuid4
+
+class BaseModel:
+    """Class"""
+
+    def __init__(self, *args, **kwargs):
+        """intialize the class
+        """
+        seif.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+
+        if (len(kwargs) == 0):
+            models.storage.new(self)
+        else:
+            for x,y in kwargs.items():
+                if (x == "created_at" or x == "updated_at"):
+                    self.__dict__[x] = datetime.strptime(y, "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[x] = y
+
+    def save(self):
+        """change updated_at to curren time"""
+        self.updated_at = datetime.today()
+        models.storage.save()
+
+    def __str__(self):
+        """print: [<class name>] (<self.id>) <self.__dict__>"""
+        classname = self.__class__.__name__
+        return "[{}] ({}) {}".format(classname,  self.id, self.__dict__)
+
+    def to_dict(self):
+        """Dictionary of the BaseModel instance"""
+        dictionary = self.__dict__.copy()
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        dictionary["__class__"] = self.__class__.__name__
+        return dictionary
