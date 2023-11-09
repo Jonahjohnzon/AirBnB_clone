3f749dcbdd932cb4631e3ec7094e94a3875a6c4a
Emedo586 <emedopeace99@gmail.com>
2023-11-08 16:05:26 -0800

    AirBnB is not a joke

--- models/__init__.py
+++ models/__init__.py
@@ -2,9 +2,6 @@
 """__init__"""
 from  models.engine.file_storage import FileStorage
 
-<<<<<<< HEAD
 
-=======
->>>>>>> bb2f9dec091095ff1f04ec725b6b934db4d63da5
 storage = FileStorage()
 storage.reload()
